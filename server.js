0bf29b24b0848ed6af3d4937d21100841981c579
jmsngrp <jon@thejamisongroup.org>
2026-04-22 15:23:00 -0500

    Disable secure cookies in server.js
    Set secure cookie option to false for all environments.

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     sameSite: 'lax'
   }
 }));
